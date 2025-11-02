3e3e39d33d90e6f2a5d31a65ce055f1b0e301354
kennedy-ak <78437251+kennedy-ak@users.noreply.github.com>
2025-11-02 23:01:13 +0000

    update

--- hitech_store/settings.py
+++ hitech_store/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = ["store.digitalrepublic.space","157.173.118.68","*"]
 
 
 # Application definition
